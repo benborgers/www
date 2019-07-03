2d7480a6d0c0541724e1e83d34e8615a4dcb081b
Ben Borgers <ben@benborgers.com>
2019-07-03 16:49:33 -0400

    Update colors.js

--- src/util/colors.js
+++ src/util/colors.js
@@ -1,11 +1,5 @@
 module.exports = {
   "text-black": "hsl(217, 70%, 15%)",
   "text-gray": "hsl(217, 10%, 40%)",
-
-  "brand": "hsl(217, 96%, 45%)",
-  "brand-light": "hsl(217, 100%, 80%)",
-
-  "background": "hsl(218, 100%, 98%)",
-
   "highlight": "hsl(218, 100%, 95%)"
 }
