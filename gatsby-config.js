fb5426bf8bcfbcbb686eeb219871353a16a856ab
benborgers <borgersbenjamin@gmail.com>
2019-07-18 21:30:00 -0400

    recreate gatsby files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  siteMetadata: {
-    title: "Ben Borgers",
-    description: "Site description.",
-    emoji: "🦕",
-    siteUrl: "https://benborgers.com"
-  },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-netlify-cms",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "cms",
-        path: __dirname + "/src/cms/"
-      }
-    },
-    "gatsby-transformer-remark"
+    "gatsby-plugin-react-helmet"
   ]
 }
