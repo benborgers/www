c3566c541f19e5d7589e98f3a1de2fa61642d08c
benborgers <borgersbenjamin@gmail.com>
2019-07-18 23:31:58 -0400

    make -webkit-tap-highlight-color transparent again

--- src/components/StyleReset.js
+++ src/components/StyleReset.js
@@ -14,6 +14,7 @@ export default () => {
           background: transparent;
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
+          -webkit-tap-highlight-color: transparent;
         }
       `}
     />
