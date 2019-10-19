41a467dc1ef3dc081f9a46b0947a326f5f8ab0c3
benborgers <borgersbenjamin@gmail.com>
2019-10-18 22:30:56 -0400

    generate share images

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 const fs = require("fs")
 const path = require("path")
+const fetch = require("node-fetch")
+
+const publishedReferences = []
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
@@ -32,13 +35,81 @@ exports.createPages = async ({ actions }) => {
     const slug = file.slice(0, -5) // remove the .json ending, so 5 characters
 
     if(post.published === "yes") {
+      const postPlus = { ...post, slug }
+
+      publishedReferences.push(postPlus)
+
       createPage({
         path: "/reference/" + slug,
         component: path.resolve("./src/templates/reference.js"),
         context: {
-          post: { ...post, slug } 
+          post: postPlus
         }
       })
     }
   }
+}
+
+exports.onPostBuild = () => {
+  const postsToGet = publishedReferences.length
+  let postsGot = 0
+
+  const clean = text => encodeURIComponent(text)
+
+  return new Promise(resolve => {
+     /* Fetch share images for reference pages */
+
+    for(const post of publishedReferences) {
+      const html = clean(`
+        <main>
+          <div>
+            <p>${post.title}</p>
+          </div>
+        </main>
+      `)
+  
+      const css = clean(`
+        @import url("https://rsms.me/inter/inter.css");
+
+        * {
+          margin: 0;
+          padding: 0;
+        }
+
+        main {
+          height: 100vh;
+          display: grid;
+          place-items: center center;
+        }
+
+        div {
+          margin: 0 96px;
+        }
+
+        p {
+          font-family: "Inter", sans-serif;
+          color: hsl(200, 29%, 16%);
+          font-size: 40px;
+          font-weight: 600;
+          border-left: 8px solid hsl(53, 100%, 50%);
+          padding-left: 32px;
+        }
+      `)
+  
+      fetch(`https://og-image.glitch.me/?html=${html}&css=${css}`)
+        .then(res => {
+          const dest = fs.createWriteStream(`./public/reference/${post.slug}.png`)
+  
+          res.body.pipe(dest)
+
+          dest.on("finish", () => {
+            postsGot++
+            if(postsGot === postsToGet) {
+              resolve(true)
+            }
+          })
+        })
+    }
+
+  })
 }
