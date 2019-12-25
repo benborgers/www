b7904fd7fc80e086994c2bc191c81e2b685821af
benborgers <borgersbenjamin@gmail.com>
2019-12-25 11:44:09 -0500

    crosspost blog posts to DEV

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,86 @@ const createDir = dirName => {
   }
 }
 
+exports.onCreatePage = ({ page }) => new Promise(resolve => {
+  const doNotCrosspost = ["emojicdn",
+                          "gatsby-last-built"]
+
+  if(page.path.startsWith("/blog/") && page.context.frontmatter) {
+    const frontmatter = page.context.frontmatter
+  
+    const title = frontmatter.title
+    const description = frontmatter.description
+    
+    const id = page.path.match(/\/blog\/(?<id>.*)\//).groups.id
+    
+    const body = fs.readFileSync(`./src/pages/blog/${id}.mdx`, "utf8").split("---")[2]
+    const preBody = "\n*This article was originally posted on [my personal blog](https://benborgers.com/blog).*\n\n"
+    const bodyMarkdown = `---
+    title: ${title}
+    description: ${description}
+    cover_image: https://benborgers.com/assets/${id}.png
+    published: true
+    canonical_url: https://benborgers.com/blog/${id}/
+    ---
+    ` + preBody + body
+
+    if(doNotCrosspost.includes(id)) {
+      console.log(`DEV: skipping ${id}`)
+      resolve()
+      return
+    }
+
+    fetch("https://dev.to/api/articles/me", {
+      headers: {
+        "api-key": process.env.DEV_TOKEN
+      }
+    })
+      .then(res => res.json())
+      .then(articles => {
+        const existingArticle = articles.find(a => a.title === title)
+
+        const fetchOptions = {
+          headers: {
+            "api-key": process.env.DEV_TOKEN,
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({
+            article: {
+              body_markdown: bodyMarkdown
+            }
+          })
+        }
+
+        if(!existingArticle) {
+          fetch("https://dev.to/api/articles", { ...fetchOptions, method: "POST" })
+            .then(res => res.json())
+            .then(json => {
+              if(json.error) {
+                console.log(`DEV: error crossposting ${id}`, json)
+              } else {
+                console.log(`DEV: crossposted ${json.url}`)
+              }
+              resolve()
+            })
+        } else {
+          fetch(`https://dev.to/api/articles/${existingArticle.id}`, { ...fetchOptions, method: "PUT" })
+            .then(res => res.json())
+            .then(json => {
+              if(json.error) {
+                console.log(`DEV: error updating ${id}`, json)
+              } else {
+                console.log(`DEV: updated ${json.url}`)
+              }
+
+              resolve()
+            })
+        }
+      })
+  } else {
+    resolve()
+  }
+})
+
 exports.onPostBuild = () => new Promise(resolve => {
   /* Get share images from Figma */
 
