6c10c2db60afbd6cefbe207d43e2006b1adbb41a
benborgers <borgersbenjamin@gmail.com>
2020-02-04 22:07:28 -0500

    add rss feed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,25 @@
 const path = require("path")
+const fs = require("fs")
 const fetch = require("node-fetch")
+const { Feed } = require("feed")
 
 exports.createPages = ({ actions: { createPage } }) => new Promise(resolve => {
+  const feed = new Feed({
+    title: "Ben Borgers’ Blog",
+    description: "Articles and notes on React, Gatsby, Node, and more.",
+    id: "https://benborgers.com/blog/",
+    link: "https://benborgers.com/blog/",
+    language: "en",
+    image: "https://figure.netlify.com/www-share-image",
+    generator: "Ben Borgers",
+    author: {
+      name: "Ben Borgers",
+      email: "borgersbenjamin@gmail.com",
+      link: "https://benborgers.com"
+    }
+  })
+
+
   const blogPostIndex = path.resolve("src/templates/BlogIndex.js")
   const blogPostTemplate = path.resolve("src/templates/BlogPost.js")
 
@@ -19,9 +37,23 @@ exports.createPages = ({ actions: { createPage } }) => new Promise(resolve => {
       let i = 0
 
       posts.forEach(post => {
+        if(!post.fields.Published) {
+          return console.log(`Not publishing ${post.fields.Title}`)
+        }
+
+
         fetch(`https://potion.benborgers.now.sh/api/html?id=${post.id}`)
           .then(res => res.text())
           .then(html => {
+            feed.addItem({
+              title: post.fields.Title,
+              id: post.fields.Slug,
+              link: `https://benborgers.com/blog/${post.fields.Slug}/`,
+              description: post.fields.Description,
+              content: html,
+              date: new Date(post.fields.Date.start_date)
+            })
+
             createPage({
               path: `/blog/${post.fields.Slug}/`,
               component: blogPostTemplate,
@@ -33,7 +65,10 @@ exports.createPages = ({ actions: { createPage } }) => new Promise(resolve => {
 
             i++
 
-            if(i === posts.length) resolve()
+            if(i === posts.length) {
+              fs.writeFileSync("./public/feed.xml", feed.atom1())
+              resolve()
+            }
           })
       })
     })
