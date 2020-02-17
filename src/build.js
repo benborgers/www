ddaa941c0981347fcc880d480ca10206fd0fd706
benborgers <borgersbenjamin@gmail.com>
2020-02-16 22:26:45 -0500

    begin redesign

---
+++ src/build.js
@@ -0,0 +1,58 @@
+const fs = require("fs")
+const htmlMinifier = require("html-minifier").minify
+
+const { table, html } = require("./notion")
+const constants = require("./constants")
+const components = require("./components")
+
+const writeFile = (path, contents) => {
+  const minifiedHtml = htmlMinifier(contents, {
+    collapseWhitespace: true,
+    html5: true,
+  })
+  fs.writeFile(`${constants.buildDir}/${path}`, minifiedHtml, () => {})
+}
+
+const main = async () => {
+
+  /* Home page */
+  const homeContents = await html(constants.pageId.home)
+  const homeHtml = await components.layout({
+    title: "Ben Borgers",
+    description: "",
+    ogImage: "https://figure.netlify.com/www-share-image",
+    body: `
+      <div class="page-simple">
+        <p class="title">Ben Borgers</p>
+        ${homeContents}
+      </div>
+    `
+  })
+  writeFile("index.html", homeHtml)
+
+
+  /* Projects page */
+  const projects = await table(constants.pageId.projects)
+  const projectsHtml = await components.layout({
+    title: "Projects - Ben Borgers",
+    description: "",
+    ogImage: "https://figure.netlify.com/www-share-image",
+    body: `
+      <div class="page-simple">
+        <a href="/" class="back">← benborgers.com</a>
+        <p class="title more-space">Projects</p>
+
+        ${projects.map(project => `
+          <div class="block ${project.fields.Highlight ? "highlight" : ""}">
+            <a class="primary" href=${!project.fields.Link.startsWith("http") ? "https://" : ""}${project.fields.Link}>${project.fields.Name}</a>
+            <p class="secondary">${project.fields.Description}</p>
+          </div>
+        `).join("")}
+      </div>
+    `
+  })
+  writeFile("projects.html", projectsHtml)
+
+}
+
+main()
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  buildDir: "public",
+  pageId: {
+    home: "64e69444755249ae9bd9a1c4d8357277",
+    projects: "accae8e6b5b8430cbbf62f0842fa18bc"
+  }
+}
