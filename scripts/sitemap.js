deec8ad2b2da85617eb99dda7e103100473a6b3b
benborgers <borgersbenjamin@gmail.com>
2020-03-23 19:28:37 -0400

    ping Google if sitemap has changed

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,5 +1,6 @@
 import fs from "fs"
 import { SitemapStream, streamToPromise } from "sitemap"
+import fetch from "node-fetch"
 
 import getPosts from "../helpers/getPosts"
 
@@ -7,11 +8,40 @@ if(!fs.existsSync("./public")) {
   fs.mkdirSync("./public")
 }
 
+const countInstances = (fullText, phrase) => {
+  return (fullText.match(new RegExp(phrase, "g")) || []).length
+}
+
 const main = async () => {
   const sitemap = new SitemapStream({ hostname: "https://benborgers.com" })
 
   streamToPromise(sitemap)
-    .then(file => fs.writeFileSync("./public/sitemap.xml", file.toString()))
+    .then(async file => {
+      fs.writeFileSync("./public/sitemap.xml", file.toString())
+
+      /* 
+       * Check if sitemap has the same number of items as last time. 
+       * If not, ask Google to re-index. 
+       */
+
+      const currentSitemap = await (await fetch("https://benborgers.com/sitemap.xml")).text()
+      const currentUrls = countInstances(currentSitemap, "<url>")
+
+      const newUrls = countInstances(file.toString(), "<url>")
+
+      if(currentUrls !== newUrls) {
+        fetch("http://www.google.com/ping?sitemap=https://benborgers.com/sitemap.xml")
+          .then(res => {
+            if(res.ok) {
+              console.log(`[SITEMAP] Google was pinged, since the new sitemap has ${newUrls} pages compared to ${currentUrls} pages.`)
+            } else {
+              console.log(`[SITEMAP] Error with pinging Google.`)
+            }
+          })
+      } else {
+        console.log(`[SITEMAP] The sitemap still has ${newUrls} pages, so Google was not pinged.`)
+      }
+    })
 
   sitemap.write({
     url: "/",
