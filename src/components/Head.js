38d3ebf3c3f46c2024cab9b541c391de260ca5be
benborgers <borgersbenjamin@gmail.com>
2020-04-10 20:20:09 -0400

    add og:image

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,11 @@
 import React from "react"
 import { Helmet } from "react-helmet-async"
 
+import useImage from "../hooks/useImage"
+
 export default ({ title, description }) => {
+  const image = useImage()
+
   return (
     <Helmet>
       <title>{title}</title>
@@ -11,6 +15,9 @@ export default ({ title, description }) => {
       <meta property="og:site_name" content="Ben Borgers" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://source.unsplash.com/${image}/1200x630`} />
+
+      <meta property="twitter:card" content="summary_large_image" />
 
       <link rel="icon" href="https://emojicdn.elk.sh/🐙" />
 
