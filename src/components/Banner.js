af5e8e868504dfee3edc9e315aacf3c916c5a44d
benborgers <borgersbenjamin@gmail.com>
2020-05-03 15:17:19 -0400

    add banner

---
+++ src/components/Banner.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { css, Global } from "@emotion/core"
+
+export default () => {
+  return (
+    <>
+      <Global
+        styles={css`
+          header {
+            margin-top: 32px;
+          }
+        `}
+      />
+
+      <a
+        href="https://underline.email"
+        target="_blank"
+      >
+        <div
+          css={css`
+            background-color: var(--text-500);
+            padding: 8px 16px;
+            border-radius: 8px;
+
+            p, p * {
+              line-height: 1.6;
+              color: white;
+              margin: 0;
+              text-align: center;
+            }
+
+            .underline {
+              text-decoration: underline;
+              text-decoration-color: hsl(0deg 0% 50%);
+            }
+          `}
+        >
+          <p>
+            I'm building a super simple email list app. <br />Sign up during beta and get 2,000 free emails per month, for life.
+            {" "}
+            <span className="underline">Check it out.</span>
+          </p>
+        </div>
+      </a>
+    </>
+  )
+}
