d02d1b27297647da7ca59f3061248886c743d2da
Ben Borgers <borgersbenjamin@gmail.com>
2020-11-19 10:30:09 -0500

    Use smartypants quotes in titles

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const path = require('path')
+const marked = require('marked')
+
+marked.setOptions({ smartypants: true })
 
 const markdown = require('./src/helpers/markdown')
 
@@ -9,6 +12,8 @@ exports.createPages = ({ actions: { createPage } }) => {
     fs.readdirSync('src/cms/blog').forEach(filename => {
         const file = fs.readFileSync('src/cms/blog/' + filename)
         const json = JSON.parse(file)
+        
+        json.title = marked.parseInline(json.title)
         
         if(json.published) {
             createPage({
