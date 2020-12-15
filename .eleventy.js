0e23338ee35b2e78954d0c1b8165fcc243a9185b
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-14 20:01:51 -0500

    Start styling

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,22 @@ module.exports = eleventyConfig => {
         console.log(value)
         return 'dumped'
     })
+
+    const markdownIt = require('markdown-it')
+    const markdownItOptions = {
+        html: true,
+        linkify: true,
+        typographer: true
+    }
+
+    const md = markdownIt(markdownItOptions)
+    const headingOverride = (tokens, id) => {
+        const token = tokens[id]
+        const number = Number(token.tag.substr(1))
+        return `<${token.type === 'heading_close' ? '/' : ''}h${number + 1}>`
+    }
+    md.renderer.rules.heading_open = headingOverride
+    md.renderer.rules.heading_close = headingOverride
+
+    eleventyConfig.setLibrary('md', md)
 }
---
+++ _data/nav.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        name: 'Black Mirror',
+        url: '/tags/black-mirror/'
+    },
+    {
+        name: 'Archive',
+        url: '/archive'
+    }
+]
