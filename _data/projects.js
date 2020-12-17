c60f3ea22b07c5bd79e2a75e1177739fe96e26f4
Ben Borgers <borgersbenjamin@gmail.com>
2020-12-17 16:43:21 -0500

    Build homepage

---
+++ _data/projects.js
@@ -0,0 +1,60 @@
+module.exports = {
+    'Personal projects': [
+        {
+            name: 'Blocks',
+            description: 'An app for my school’s schedule used by 2,000 students and teachers at my high school.',
+            link: 'blocks.elk.sh'
+        },
+        {
+            name: 'Bagel Institute',
+            description: 'Tools for more interactive college teaching.',
+            link: 'bagel.institute'
+        },
+        {
+            name: 'Underline',
+            description: 'A dead-simple and affordable way to run an email newsletter.',
+            link: 'underline.email'
+        },
+        {
+            name: 'Potion',
+            description: 'A reverse-engineered API for the Notion note-taking app.',
+            link: 'github.com/benborgers/potion'
+        },
+        {
+            name: 'EMOJICDN',
+            description: 'A simple API for getting images of emojis that serves over two million requests per month.',
+            link: 'emojicdn.elk.sh'
+        },
+        {
+            name: 'AP Chem',
+            description: 'Unit summaries for AP Chemistry.',
+            link: 'chem.elk.sh'
+        },
+        {
+            name: 'AP Bio',
+            description: 'Questions to prepare for the AP Biology exam.',
+            link: 'bio.elk.sh'
+        }
+    ],
+    'Work': [
+        {
+            name: 'Diyi',
+            description: 'A payment and student management platform for Diyi Boston, a school providing online courses and college help.',
+            link: 'diyiboston.com'
+        },
+        {
+            name: 'IBM Security',
+            description: 'Programming summer internship at IBM Mass Lab in the summers of 2019 and 2020.'
+        },
+        {
+            name: 'Rep.ly',
+            description: 'Implemented new features on a public Q&A platform by the agency Dip.',
+            link: 'rep.ly'
+        },
+        {
+            name: 'Sophrosyne',
+            description: 'Build an app for Sophrosyne, a mental health-related club at my high school.',
+            link: 'sophrosyne.now.sh'
+        }
+    ]
+}
