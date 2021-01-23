efdc07bc6ac7c1d3cab42ea8fc4887ca25ed8dd8
Ben Borgers <borgersbenjamin@gmail.com>
2021-01-23 17:54:30 -0500

    Get blog posts working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        typography: theme => ({
+            DEFAULT: {
+                css: {
+                    a: {
+                        color: theme('colors.blue.600'),
+                        textDecoration: 'none'
+                    },
+                    pre: {
+                        backgroundColor: theme('colors.gray.100'),
+                        color: theme('colors.gray.700')
+                    }
+                }
+            }
+        })
+    }
   },
   variants: {
     extend: {},
