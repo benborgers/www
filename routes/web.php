1c8d966ecb368fce08db70a67a5b3832e2345f2b
Ben Borgers <borgersbenjamin@gmail.com>
2021-01-23 20:27:43 -0500

    Add some more posts

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\BlogController;
 Route::get('/', [BlogController::class, 'home'])->name('home');
 Route::get('/posts', [BlogController::class, 'postsIndex'])->name('posts.index');
 Route::get('/posts/{number}', [BlogController::class, 'showPost'])->name('posts.show');
+Route::get('/blog/{slug}', fn ($slug) => redirect()->route('posts.show', $slug));
 Route::get('/sync', [BlogController::class, 'sync'])->name('sync');
