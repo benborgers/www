ee317141671bf59cc1b11fdaccda2126e3ce84af
Ben Borgers <borgersbenjamin@gmail.com>
2021-01-25 07:50:45 -0500

    Add redirect for gatsby-normalize

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\BlogController;
 Route::get('/', [BlogController::class, 'home'])->name('home');
 Route::get('/posts', [BlogController::class, 'postsIndex'])->name('posts.index');
 Route::get('/posts/{number}', [BlogController::class, 'showPost'])->name('posts.show');
+
 Route::get('/blog/{slug}', fn ($slug) => redirect()->route('posts.show', $slug));
+Route::redirect('/reference/how-to-add-normalize-css-to-gatsby', '/posts/16');
 
 Route::get('/admin', fn () => response()->json(array_reverse(Cache::get('404') ?? [])) );
