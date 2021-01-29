f3987f5f9f75e4f856a2062dd8ca8f01af754cb7
Ben Borgers <borgersbenjamin@gmail.com>
2021-01-29 08:52:22 -0500

    Write text on 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,13 +1,14 @@
 <x-layouts.blog>
-    <h1 class="bg-gradient-to-tr from-red-300 to-blue-400 bg-clip-text text-transparent font-bold text-center text-3xl sm:text-4xl">
-        four-oh-four
-        <br class="sm:hidden" />
-        not found
-    </h1>
+    <div class="mb-8">
+        <h1 class="bg-gradient-to-tr from-red-300 to-blue-400 bg-clip-text text-transparent font-bold text-center text-3xl sm:text-4xl">
+            four-oh-four
+            <br class="sm:hidden" />
+            not found
+        </h1>
+    </div>
 
-    @php
-        $array = Cache::get('404') ?? [];
-        $array[] = '/' . request()->path();
-        Cache::forever('404', $array);
-    @endphp
+    <p class="text-lg text-gray-500">
+        If you expected something to be here, I’d really appreciate if you emailed me to let me know:
+        <a href="mailto:benborgers@hey.com" class="text-blue-600 font-medium">benborgers@hey.com</a>. Thanks in advance!
+    </p>
 </x-layouts.blog>
