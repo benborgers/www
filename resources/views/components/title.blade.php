cf7a6b6dedb784ea09f2c1bf49560fb1196b614d
Ben Borgers <borgersbenjamin@gmail.com>
2021-02-10 21:39:36 -0500

    I'm switching it up again

--- resources/views/components/title.blade.php
+++
@@ -1,3 +0,0 @@
-<h1 class="text-3xl sm:text-4xl font-black text-gray-800 sm:leading-tight">
-    {{ $slot }}
-</h1>
--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-const mix = require('laravel-mix')
-
-mix.disableNotifications()
-
-mix.postCss('resources/css/style.css', 'public/css', [
-    require('tailwindcss'),
-    require('autoprefixer')
-])
