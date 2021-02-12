f049184d5d6773d4b196f895434fc80c0a7fb0ba
Ben Borgers <borgersbenjamin@gmail.com>
2021-02-12 14:37:58 -0500

    Refresh build in serve.js

--- serve.js
+++ serve.js
@@ -1,8 +1,13 @@
 const serveHandler = require('serve-handler')
 const http = require('http')
+const { spawn } = require('child_process')
 
 http.createServer((req, res) => serveHandler(req, res, {
     public: './public'
 })).listen(3000)
 
 console.log('http://localhost:3000')
+
+setInterval(() => {
+    spawn('npm run build', { shell: true })
+}, 2000)
