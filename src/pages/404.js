765f85005811f0c20604b878b798814440d155bb
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-06 22:22:20 -0400

    Create 404 that reports pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import fetch from 'node-fetch'
+import React, { useEffect } from 'react'
+import { Link, useLocation } from '../components/Router'
 
-export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
-  </div>
-)
+export default () => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        fetch('https://friede.gg/api/sheets/append', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+                id: '1bq41L9JuxWzsx_xdxGLamLg0vlhZSMA8Tijr-tbBb0w',
+                sheet: 'Sheet',
+                rows: [{
+                    Timestamp: (new Date()).toLocaleString('en-US'),
+                    Path: pathname
+                }]
+            })
+        })
+            .then(res => res.text())
+            .then(json => console.log(json))
+    })
+
+    return (
+        <div className="p-4 sm:p-8 text-center">
+            <h1 className="text-2xl font-extrabold text-gray-900 mb-2">page not found — sorry!</h1>
+            <div>
+                <Link to="/" className="text-rose-600 underline font-semibold">back to the homepage</Link>
+            </div>
+        </div>
+    )
+}
