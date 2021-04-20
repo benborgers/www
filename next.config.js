a67f0d422ed0f023f7b6dfffe2c5e513913b0f08
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-20 09:34:35 -0400

    Add Notion pages to sitemap

--- next.config.js
+++ next.config.js
@@ -5,15 +5,13 @@ module.exports = {
                 source: '/blog/:slug',
                 destination: '/posts/:slug',
                 permanent: true
+            },
+            {
+                source: '/a81d0c09-5d6f-4310-baf6-2fc2938b89d2',
+                destination: '/',
+                permanent: true
             }
         ]
-    },
-    webpack: (config, { isServer }) => {
-        if(isServer) {
-            require('./scripts/generate-sitemap')
-        }
-
-        return config
     },
     future: {
         webpack5: true
--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,10 +1,13 @@
-const fs = require('fs');
+const fs = require('fs')
+const notion = require('../data/notion');
 
 (async () => {
     const posts = fs.readdirSync('data/posts').map(filename => `/posts/${filename.replace(/\.md$/, '')}`)
+    const notionPages = Object.keys(notion).map(id => `/${id}`)
 
     const pages = [
-        ...posts
+        ...posts,
+        ...notionPages
     ]
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
