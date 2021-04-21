981c2c13aab714bbc718ef38205c716b3f98a659
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-21 16:48:38 -0400

    Use redirect map

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 module.exports = {
     redirects() {
-        return [
-            {
-                source: '/blog/:slug',
-                destination: '/posts/:slug',
+        const redirects = {
+            '/blog/:slug': '/posts/:slug',
+            '/a81d0c09-5d6f-4310-baf6-2fc2938b89d2': '/',
+            '/readme/embroidery': '/1847bacf-5ef0-42db-b053-f70248f54362'
+        }
+
+        const output = []
+
+        for(const source in redirects) {
+            output.push({
+                source,
+                destination: redirects[source],
                 permanent: true
-            },
-            {
-                source: '/a81d0c09-5d6f-4310-baf6-2fc2938b89d2',
-                destination: '/',
-                permanent: true
-            }
-        ]
+            })
+        }
+
+        return output
     },
     future: {
         webpack5: true
