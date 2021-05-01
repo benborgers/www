9d29bf78e6b4606c2bde9b4f1f7b6f4a84a7dfc7
Ben Borgers <borgersbenjamin@gmail.com>
2021-05-01 11:12:41 -0400

    Improve purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: ['./pages/**/*.js', './components/**/*.js', './mdx/**/*.mdx'],
+    purge: ['./**/*.{js,jsx,mdx}'],
     darkMode: false,
     theme: {
         extend: {
