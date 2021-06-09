5091c640ca46f326e7e47659be2722c9f9cf6217
Ben Borgers <borgersbenjamin@gmail.com>
2021-06-09 19:36:34 -0400

    Use Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 export default {
-  buildOptions: {
-    site: 'http://benborgers.com',
-    sitemap: true,
-  },
-  devOptions: {
-    // port: 3000,         // The port to run the dev server on.
-    // tailwindConfig: '', // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-  },
-};
+    buildOptions: {
+        site: 'http://benborgers.com',
+        sitemap: true
+    },
+    devOptions: {
+        tailwindConfig: './tailwind.config.js'
+    }
+}
