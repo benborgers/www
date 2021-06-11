65b9cd58f72587864e2fc32b500d51471bfa0eff
Ben Borgers <borgersbenjamin@gmail.com>
2021-06-11 11:45:49 -0400

    Redesign 404 to be macOS-inspired

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                orange: colors.orange,
+                pink: colors.pink,
+                rose: colors.rose
+            }
+        }
+    },
     mode: 'jit',
     purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
     plugins: [require('@tailwindcss/typography')]
