6b0716f8d624f925b34abfe68b87842a7fffeff9
Ben Borgers <borgersbenjamin@gmail.com>
2021-08-11 20:31:13 -0400

    Create Head component

---
+++ components/Head.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function CustomHead({ title }) {
+    return (
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="https://emojicdn.elk.sh/🐙" />
+            {process.env.NODE_ENV === 'production' &&
+                <script src="https://anteater.benborgers.com/script.js" data-site="ZWCPJCUA" defer></script>
+            }
+        </Head>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'components/Head'
+
 const ProjectCard = ({ emoji, background, title, subtitle, link }) => {
     return (
         <a
@@ -24,145 +26,149 @@ const ProjectCard = ({ emoji, background, title, subtitle, link }) => {
 
 export default function Index() {
     return (
-        <div className="p-4 sm:p-6 pb-36 sm:pb-36">
-            <div className="flex justify-end">
-                <a href="/posts" className="block text-gray-400 underline">Blog</a>
-            </div>
+        <>
+            <Head title="Ben Borgers" />
 
-            <div className="max-w-screen-lg mx-auto">
-                <div className="mt-6 lg:mt-32">
-                    <p className="text-indigo-400 text-sm font-bold">
-                        <img src="https://emojicdn.elk.sh/🏝" className="inline-block mr-2 w-4 transform -translate-y-0.5" />
-                        <span>welcome to my island on the internet</span>
-                    </p>
-                    <h1 className="text-2xl md:text-4xl leading-snug md:leading-snug text-gray-800 mt-1">
-                        I’m Ben Borgers, an 18 year-old developer from Boston, MA.
-                        In the fall, I’m going to Tufts University to study computer science.
-                    </h1>
+            <div className="p-4 sm:p-6 pb-36 sm:pb-36">
+                <div className="flex justify-end">
+                    <a href="/posts" className="block text-gray-400 underline">Blog</a>
+                </div>
 
-                    <div className="mt-4 flex space-x-3">
-                        {[
-                            {
-                                label: 'email: ben@elk.sh',
-                                link: 'mailto:ben@elk.sh'
-                            },
-                            {
-                                label: 'twitter',
-                                link: 'https://twitter.com/benborgers'
-                            },
-                            {
-                                label: 'github',
-                                link: 'https://github.com/benborgers'
-                            }
-                        ].map(({ label, link }) => (
-                            <a
-                                className="block bg-indigo-50 px-4 py-1 rounded-full text-indigo-500 font-semibold text-sm"
-                                href={link}
-                                target="_blank"
-                                rel="noreferrer"
-                                key={label}
-                            >
-                                {label}
-                            </a>
-                        ))}
+                <div className="max-w-screen-lg mx-auto">
+                    <div className="mt-6 lg:mt-32">
+                        <p className="text-indigo-400 text-sm font-bold">
+                            <img src="https://emojicdn.elk.sh/🏝" className="inline-block mr-2 w-4 transform -translate-y-0.5" />
+                            <span>welcome to my island on the internet</span>
+                        </p>
+                        <h1 className="text-2xl md:text-4xl leading-snug md:leading-snug text-gray-800 mt-1">
+                            I’m Ben Borgers, an 18 year-old developer from Boston, MA.
+                            In the fall, I’m going to Tufts University to study computer science.
+                        </h1>
+
+                        <div className="mt-4 flex space-x-3">
+                            {[
+                                {
+                                    label: 'email: ben@elk.sh',
+                                    link: 'mailto:ben@elk.sh'
+                                },
+                                {
+                                    label: 'twitter',
+                                    link: 'https://twitter.com/benborgers'
+                                },
+                                {
+                                    label: 'github',
+                                    link: 'https://github.com/benborgers'
+                                }
+                            ].map(({ label, link }) => (
+                                <a
+                                    className="block bg-indigo-50 px-4 py-1 rounded-full text-indigo-500 font-semibold text-sm"
+                                    href={link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    key={label}
+                                >
+                                    {label}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="mt-24 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-8 space-y-6 md:space-y-0">
+                        <h2 className="text-xl font-bold text-gray-900 col-span-2">Projects</h2>
+
+                        <ProjectCard
+                            emoji="🗝"
+                            background="bg-gray-100"
+                            title="Vault"
+                            subtitle="Password manager for myself and friends."
+                            link="https://vault.elk.sh"
+                        />
+
+                        <ProjectCard
+                            emoji="🥣"
+                            background="bg-blue-100"
+                            title="Cornflakes"
+                            subtitle="Simple and privacy-focused email newsletter tool."
+                            link="https://cornflakes.app"
+                        />
+
+                        <ProjectCard
+                            emoji="🎒"
+                            background="bg-red-100"
+                            title="Blocks"
+                            subtitle="App for my high school’s schedule used by 2,000 students and teachers."
+                            link="https://blocks.elk.sh"
+                        />
+
+                        <ProjectCard
+                            emoji="🥯"
+                            background="bg-orange-100"
+                            title="Bagel Institute"
+                            subtitle="Tools for more interactive college teaching."
+                            link="https://bagel.institute"
+                        />
+
+                        <ProjectCard
+                            emoji="🧪"
+                            background="bg-green-100"
+                            title="Potion"
+                            subtitle="Open-source reverse-engineered API for the Notion note taking app."
+                            link="https://potion-api.vercel.app"
+                        />
+
+                        <ProjectCard
+                            emoji="🥳"
+                            background="bg-yellow-100"
+                            title="EMOJICDN"
+                            subtitle="API for getting images of emojis that serves two million requests per month."
+                            link="https://emojicdn.elk.sh"
+                        />
+
+
+                        <h2 className="text-xl font-bold text-gray-900 col-span-2 mt-8">Work</h2>
+
+                        <ProjectCard
+                            emoji="📨"
+                            background="bg-blue-100"
+                            title="Buttondown"
+                            subtitle="Frontend development using Vue."
+                            link="https://buttondown.email"
+                        />
+
+                        <ProjectCard
+                            emoji="✏️"
+                            background="bg-yellow-100"
+                            title="Diyi"
+                            subtitle="Payment and student management system for an online school in Boston."
+                            link="https://diyiboston.com"
+                        />
+
+                        <ProjectCard
+                            emoji="🐝"
+                            background="bg-gray-100"
+                            title="IBM Security"
+                            subtitle="Software engineering internship in summer 2019 and 2020, working on a new security offering for enterprise."
+                            link="https://www.ibm.com/products/guardium-insights"
+                        />
+
+                        <ProjectCard
+                            emoji="💬"
+                            background="bg-gray-200"
+                            title="Rep.ly"
+                            subtitle="Implemented new features using React."
+                            link="https://rep.ly"
+                        />
+
+                        <ProjectCard
+                            emoji="🧠"
+                            background="bg-pink-100"
+                            title="Sophrosyne"
+                            subtitle="Built a PWA for a mental health awareness club."
+                            link="https://sophrosyne.vercel.app"
+                        />
                     </div>
                 </div>
-
-                <div className="mt-24 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-8 space-y-6 md:space-y-0">
-                    <h2 className="text-xl font-bold text-gray-900 col-span-2">Projects</h2>
-
-                    <ProjectCard
-                        emoji="🗝"
-                        background="bg-gray-100"
-                        title="Vault"
-                        subtitle="Password manager for myself and friends."
-                        link="https://vault.elk.sh"
-                    />
-
-                    <ProjectCard
-                        emoji="🥣"
-                        background="bg-blue-100"
-                        title="Cornflakes"
-                        subtitle="Simple and privacy-focused email newsletter tool."
-                        link="https://cornflakes.app"
-                    />
-
-                    <ProjectCard
-                        emoji="🎒"
-                        background="bg-red-100"
-                        title="Blocks"
-                        subtitle="App for my high school’s schedule used by 2,000 students and teachers."
-                        link="https://blocks.elk.sh"
-                    />
-
-                    <ProjectCard
-                        emoji="🥯"
-                        background="bg-orange-100"
-                        title="Bagel Institute"
-                        subtitle="Tools for more interactive college teaching."
-                        link="https://bagel.institute"
-                    />
-
-                    <ProjectCard
-                        emoji="🧪"
-                        background="bg-green-100"
-                        title="Potion"
-                        subtitle="Open-source reverse-engineered API for the Notion note taking app."
-                        link="https://potion-api.vercel.app"
-                    />
-
-                    <ProjectCard
-                        emoji="🥳"
-                        background="bg-yellow-100"
-                        title="EMOJICDN"
-                        subtitle="API for getting images of emojis that serves two million requests per month."
-                        link="https://emojicdn.elk.sh"
-                    />
-
-
-                    <h2 className="text-xl font-bold text-gray-900 col-span-2 mt-8">Work</h2>
-
-                    <ProjectCard
-                        emoji="📨"
-                        background="bg-blue-100"
-                        title="Buttondown"
-                        subtitle="Frontend development using Vue."
-                        link="https://buttondown.email"
-                    />
-
-                    <ProjectCard
-                        emoji="✏️"
-                        background="bg-yellow-100"
-                        title="Diyi"
-                        subtitle="Payment and student management system for an online school in Boston."
-                        link="https://diyiboston.com"
-                    />
-
-                    <ProjectCard
-                        emoji="🐝"
-                        background="bg-gray-100"
-                        title="IBM Security"
-                        subtitle="Software engineering internship in summer 2019 and 2020, working on a new security offering for enterprise."
-                        link="https://www.ibm.com/products/guardium-insights"
-                    />
-
-                    <ProjectCard
-                        emoji="💬"
-                        background="bg-gray-200"
-                        title="Rep.ly"
-                        subtitle="Implemented new features using React."
-                        link="https://rep.ly"
-                    />
-
-                    <ProjectCard
-                        emoji="🧠"
-                        background="bg-pink-100"
-                        title="Sophrosyne"
-                        subtitle="Built a PWA for a mental health awareness club."
-                        link="https://sophrosyne.vercel.app"
-                    />
-                </div>
             </div>
-        </div>
+        </>
     )
 }
