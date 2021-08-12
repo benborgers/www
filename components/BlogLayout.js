794bc59d2ab86f260013e343536aff952f57ec91
Ben Borgers <borgersbenjamin@gmail.com>
2021-08-11 21:12:59 -0400

    Fix lint

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -1,11 +1,15 @@
+import Link from 'next/link'
+
 export default function BlogLayout({ children }) {
     return (
         <div className="md:grid p-4 md:pt-12 pb-24 gap-8 grid-cols-[1fr,minmax(0,65ch),1fr]">
             <div className="justify-self-end mt-1 flex justify-between md:block">
                 <p>
-                    <a href="/" className="font-medium text-gray-500 hover:text-blue-600 transition-colors duration-200">
-                        Ben&nbsp;Borgers
-                    </a>
+                    <Link href="/">
+                        <a className="font-medium text-gray-500 hover:text-blue-600 transition-colors duration-200">
+                            Ben&nbsp;Borgers
+                        </a>
+                    </Link>
                 </p>
                 <p>
                     <a
