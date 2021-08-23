b1f88e10086467a60ee25f17a16aa0c3fb72735c
Ben Borgers <borgersbenjamin@gmail.com>
2021-08-22 20:40:23 -0400

    Add email link to sidebar of `BlogLayout`

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -5,7 +5,7 @@ export default function BlogLayout({ children }) {
     return (
         <div className="md:grid p-4 md:pt-12 pb-24 gap-8 grid-cols-[1fr,minmax(0,65ch),1fr]">
             <div className="justify-self-end mt-1 flex justify-between md:block">
-                <p>
+                <p className="mb-1">
                     <Link href="/">
                         <a className="font-medium text-gray-500 hover:text-blue-600 transition-colors duration-200">
                             Ben&nbsp;Borgers
@@ -17,10 +17,19 @@ export default function BlogLayout({ children }) {
                         href="https://twitter.com/benborgers"
                         target="_blank"
                         rel="noreferrer"
-                        className="text-gray-400 hover:text-blue-600 transition-colors duration-200"
+                        className="block text-gray-400 hover:text-blue-600 transition-colors duration-200"
                         onClick={() => Fathom.trackGoal('E3LD3YS1', 0)}
                     >
-                        ⇢ Twitter
+                        🐦&nbsp;&nbsp;Twitter
+                    </a>
+
+                    <a
+                        href="mailto:ben@elk.sh"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="block text-gray-400 hover:text-blue-600 transition-colors duration-200"
+                    >
+                        📨&nbsp;&nbsp;ben@elk.sh
                     </a>
                 </p>
             </div>
