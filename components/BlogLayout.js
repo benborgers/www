4ec96d898fd66ff19af1688305241c2a74d0ddf2
Ben Borgers <borgersbenjamin@gmail.com>
2021-08-22 20:48:56 -0400

    Make emojis in BlogLayout emojicdn

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
 import Link from 'next/link'
 import * as Fathom from 'fathom-client'
 
 export default function BlogLayout({ children }) {
     return (
-        <div className="md:grid p-4 md:pt-12 pb-24 gap-8 grid-cols-[1fr,minmax(0,65ch),1fr]">
+        <div className="md:grid p-4 md:pt-12 pb-24 gap-x-10 grid-cols-[1fr,minmax(0,65ch),1fr]">
             <div className="justify-self-end mt-1 flex justify-between md:block">
                 <p className="mb-1">
                     <Link href="/">
@@ -17,19 +19,21 @@ export default function BlogLayout({ children }) {
                         href="https://twitter.com/benborgers"
                         target="_blank"
                         rel="noreferrer"
-                        className="block text-gray-400 hover:text-blue-600 transition-colors duration-200"
+                        className="text-gray-400 hover:text-blue-600 transition-colors duration-200 flex items-center space-x-2"
                         onClick={() => Fathom.trackGoal('E3LD3YS1', 0)}
                     >
-                        🐦&nbsp;&nbsp;Twitter
+                        <img src="https://emojicdn.elk.sh/🐦" alt="🐦" className="h-4" />
+                        <span>Twitter</span>
                     </a>
 
                     <a
                         href="mailto:ben@elk.sh"
                         target="_blank"
                         rel="noreferrer"
-                        className="block text-gray-400 hover:text-blue-600 transition-colors duration-200"
+                        className="text-gray-400 hover:text-blue-600 transition-colors duration-200 flex items-center space-x-2"
                     >
-                        📨&nbsp;&nbsp;ben@elk.sh
+                        <img src="https://emojicdn.elk.sh/📨" alt="📨" className="h-4" />
+                        <span>ben@elk.sh</span>
                     </a>
                 </p>
             </div>
