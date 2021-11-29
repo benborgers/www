ee46a67070df7758243dab429d548246483db838
Ben Borgers <borgersbenjamin@gmail.com>
2021-11-28 21:46:39 -0500

    Add `sanity/`

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,61 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import React from "react";
+const mathIcon = () => <span style={{ fontWeight: "bold" }}>∑</span>;
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([
+    {
+      title: "Post",
+      name: "post",
+      type: "document",
+      fields: [
+        {
+          title: "Title",
+          name: "title",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Date",
+          name: "date",
+          type: "date",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Slug",
+          name: "slug",
+          type: "slug",
+          options: {
+            source: "title",
+          },
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: "Tag",
+          name: "tag",
+          type: "string",
+          options: {
+            list: ["technical"],
+          },
+        },
+        {
+          title: "Body",
+          name: "body",
+          type: "array",
+          of: [
+            {
+              type: "block",
+              of: [{ type: "latex", title: "Inline Math", icon: mathIcon }],
+            },
+            { type: "code" },
+            { type: "image" },
+            { type: "latex", title: "Block Math", icon: mathIcon },
+          ],
+        },
+      ],
+    },
+  ]),
+});
