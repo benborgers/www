5c74e961569db64e9b8c106512a5acee82611f44
Ben Borgers <borgersbenjamin@gmail.com>
2021-12-15 22:07:45 -0500

    List out posts

--- app/routes/__standard/posts.jsx
+++ app/routes/__standard/posts.jsx
@@ -1,7 +1,26 @@
+import { json, Link, useLoaderData } from "remix";
+import getPosts from "~/lib/getPosts.server";
+
 export let meta = () => ({
-  title: "Ben Borgers’ Blog",
+  title: "Ben Borgers’ blog posts",
 });
 
+export async function loader() {
+  return json({
+    posts: await getPosts(),
+  });
+}
+
 export default function () {
-  return <div>blog</div>;
+  const data = useLoaderData();
+
+  return (
+    <>
+      {data.posts.map((data) => (
+        <Link to={`/posts/${data.slug}`} key={data.slug} className="block">
+          {data.title}
+        </Link>
+      ))}
+    </>
+  );
 }
