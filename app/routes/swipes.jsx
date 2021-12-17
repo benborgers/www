0addb1cbe1d28dbf27982565b0a95232403cffe6
Ben Borgers <borgersbenjamin@gmail.com>
2021-12-17 10:37:59 -0500

    Add live marker

--- app/routes/swipes.jsx
+++ app/routes/swipes.jsx
@@ -26,7 +26,14 @@ export default function () {
   return (
     <div className="p-4 text-center h-[90vh] grid place-items-center">
       <div>
-        <p className="text-3xl font-black text-gray-900">
+        <div className="max-w-max mx-auto px-3 py-1 rounded-full bg-red-500 flex items-center space-x-1.5">
+          <div className="h-2 w-2 rounded-full bg-white animate-pulse" />
+          <p className="text-uppercase text-white text-sm font-semibold">
+            Live
+          </p>
+        </div>
+
+        <p className="mt-4 text-3xl font-black text-gray-900">
           I have used{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-br from-sky-400 to-blue-600">
             {400 - data.swipesLeft}/400
