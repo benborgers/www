c1c7ebadb2b62e9c92edd61c89fe8ef0aad13025
Ben Borgers <borgersbenjamin@gmail.com>
2021-12-29 10:12:43 -0500

    wip tontine

--- app/lib/launchBrowser.server.ts
+++ app/lib/launchBrowser.server.ts
@@ -1,11 +1,11 @@
 import puppeteer from "puppeteer";
 
-export default async function launchBrowser() {
+export default async function launchBrowser(debug = false) {
   // Turn `headless` to `false` for debugging.
   const browser = await puppeteer.launch({
     // These args are to avoid running out of memory: https://stackoverflow.com/a/62396078
     args: ["--no-sandbox", "--no-zygote", "--single-process"],
-    headless: true,
+    headless: debug ? false : true,
   });
 
   return browser;
---
+++ app/routes/tontine.tsx
@@ -0,0 +1,42 @@
+import type { ActionFunction } from "remix";
+import launchBrowser from "~/lib/launchBrowser.server";
+
+export const action: ActionFunction = async () => {
+  const browser = await launchBrowser(true);
+  const page = await browser.newPage();
+  // Tontine shows different UI on mobile, so force desktop.
+  await page.setViewport({ width: 1200, height: 800 });
+
+  await page.goto("https://tontine.cash");
+  await page.waitForSelector("button");
+  await page.waitForTimeout(2000); // So MSCHF cover will go away.
+  await page.evaluate(() => {
+    document.querySelectorAll("button").forEach((button) => {
+      if (button.innerText.toLowerCase().includes("login")) {
+        button.click();
+      }
+    });
+  });
+
+  await page.type('input[placeholder="email"]', "ben@elk.sh");
+  await page.type(
+    'input[placeholder="password"]',
+    process.env.TONTINE_PASSWORD as string
+  );
+  await page.click("button.login-btn");
+  await page.waitForTimeout(1000);
+
+  await page.evaluate(() => {
+    document.querySelectorAll("button").forEach((button) => {
+      if (button.innerText.toLowerCase().includes("alive")) {
+        button.click();
+      }
+    });
+  });
+
+  await page.waitForTimeout(3000);
+
+  await browser.close();
+
+  return null;
+};
