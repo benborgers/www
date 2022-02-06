fca21532c2a08e20d267c3c74a2a07109f9e181d
Ben Borgers <borgersbenjamin@gmail.com>
2022-02-05 22:01:53 -0500

    Start over with Astrogit branch

--- functions/[[path]].js
+++
@@ -1,9 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-
-// @ts-ignore
-import * as build from "../build";
-
-export const onRequest = createPagesFunctionHandler({
-  build,
-  getLoadContext: (context) => context,
-});
