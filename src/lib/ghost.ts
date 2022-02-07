e5900e5f8c054ba3f4a377048a921de93d94569f
Ben Borgers <borgersbenjamin@gmail.com>
2022-02-07 15:15:50 -0500

    Log Ghost queries

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,4 +1,5 @@
 export default async function (path) {
+  console.log(`Ghost query: "${path}"`);
   const res = await fetch(
     `https://write.benborgers.com/ghost/api/v3/content/${path}?key=8e5c0f45a4e338ab1190f9ef61`
   );
