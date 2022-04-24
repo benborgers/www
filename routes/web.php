d97cfaef4e2e945651a0fd0fc67e675517c5e31c
Ben Borgers <borgersbenjamin@gmail.com>
2022-04-24 18:54:01 -0400

    Give routes names

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ Route::get('/', function () {
     return view('index', [
         'content' => Blob::firstWhere('name', 'index')->data['content']
     ]);
-});
+})->name('index');
 
 Route::get('/posts', function () {
     $posts = all_posts();
 
     return view('posts.index', ['posts' => $posts]);
-});
+})->name('posts.index');
