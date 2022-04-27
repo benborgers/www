1fd320b8ab33d4676bf2dbedd30d001c037d0c44
Ben Borgers <borgersbenjamin@gmail.com>
2022-04-26 22:38:17 -0400

    Mobile nav

--- resources/views/components/layouts/blog.blade.php
+++ resources/views/components/layouts/blog.blade.php
@@ -5,8 +5,7 @@
 @endpush
 
 <div class="font-public-sans">
-    {{-- Desktop nav --}}
-    <nav class="hidden sm:block h-screen fixed inset-y-0 left-0 bg-zinc-100 pt-16 w-16 space-y-6">
+    <nav class="bg-zinc-100 p-3 sm:p-0 sm:pt-16 mb-2 m-4 sm:m-0 rounded-xl sm:rounded-none flex sm:block gap-x-12 sm:gap-x-0 justify-center sm:h-screen sm:fixed sm:inset-y-0 sm:left-0 sm:w-16 sm:space-y-6">
         <a href="{{ route('index') }}" class="block group">
             <svg
                 @class([
