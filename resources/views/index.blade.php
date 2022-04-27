42609219752ccae33e172b5131dfb3ebf1158a03
Ben Borgers <borgersbenjamin@gmail.com>
2022-04-26 22:53:27 -0400

    Add picture of my face

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,14 @@
 <x-layouts.app title="Ben Borgers">
     <x-layouts.blog>
-        <h1 class="mb-2 text-lg font-black font-fraunces">Hi! I’m Ben Borgers</h1>
+        <div class="mb-2 space-y-2">
+            <img
+                src="{{ asset('img/me.jpg') }}"
+                class="h-24 w-auto rounded-full md:-ml-2"
+            />
+
+            <h1 class="text-lg font-black font-fraunces">Hi! I’m Ben Borgers.</h1>
+        </div>
+
         <x-prose.blog>
             <x-markdown>
                 {!! $content !!}
