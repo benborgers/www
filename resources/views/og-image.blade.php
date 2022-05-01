ab52ed7cb00f0e432965fd91a677250b0afa10f6
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-01 12:04:26 -0400

    Fix font on og-image

--- resources/views/og-image.blade.php
+++ resources/views/og-image.blade.php
@@ -1,7 +1,8 @@
 {{-- og:image --}}
 
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+{{-- Using Google Fonts so we can tell it not to swap fonts, forcing 11ty screenshot to fully load font. --}}
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900" rel="stylesheet">
 
 <div class="w-[1200px] h-[630px] font-inter bg-zinc-900 p-16 grid grid-rows-[1fr,max-content]">
     <p class="text-zinc-400 text-2xl font-medium">benborgers.com</p>
