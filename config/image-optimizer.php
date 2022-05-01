f4b86a12b838f5446a67af372043f52db608539e
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-01 15:37:00 -0400

    Tiny images, try to fix rotation

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -16,9 +16,9 @@ return [
 
         Jpegoptim::class => [
             '-m85', // set maximum quality to 85%
-            '--strip-all',  // this strips out all text information such as comments and EXIF data
+             // '--strip-all',  // this strips out all text information such as comments and EXIF data
             '--all-progressive',  // this will make sure the resulting image is a progressive one
-            '--size=1000' // 1 megabyte
+            '--size=200' // 1 megabyte
         ],
 
         Pngquant::class => [
