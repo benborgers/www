327508c75335e0b0c1509d90cae75ca50e462eae
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-01 12:34:31 -0400

    Install Horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('telescope:prune --hours=168')->daily();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
