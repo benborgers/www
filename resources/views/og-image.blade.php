defab763c2c5cb541549f3555b08bafda2c6cf42
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-02 20:24:22 -0400

    Make og:image text bigger and benborgers.com brighter

--- resources/views/og-image.blade.php
+++ resources/views/og-image.blade.php
@@ -5,6 +5,6 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900" rel="stylesheet">
 
 <div class="w-[1200px] h-[630px] font-inter bg-zinc-900 p-16 grid grid-rows-[1fr,max-content]">
-    <p class="text-zinc-400 text-2xl font-medium">benborgers.com</p>
-    <p class="font-black text-white text-7xl tracking-tight max-w-[1000px]">{{ $title }}</p>
+    <p class="text-zinc-300 text-3xl font-medium">benborgers.com</p>
+    <p class="font-black text-white text-8xl tracking-tight max-w-[1000px]">{{ $title }}</p>
 </div>
