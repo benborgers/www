d78e36886d80a9a860f7370f835b619571a2f8a5
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-05 16:23:08 -0400

    Remove filament CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,11 @@ const mix = require("laravel-mix");
 
 mix.disableNotifications();
 
-mix.js("resources/js/app.js", "public/js")
-    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
-    .postCss("resources/css/filament.css", "public/css");
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [require("tailwindcss")]
+);
 
 if (mix.inProduction()) {
     mix.version();
