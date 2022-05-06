ae9425313e61a0f5db9a665c91c67c9c96918ed3
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-05 23:31:49 -0400

    Make pages dark

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,6 +1,12 @@
-<x-layouts.app :title="$page['title']" bg="bg-white font-sf-rounded">
-    <div class="prose prose-stone">
-        {!! $page['html'] !!}
+<x-layouts.app :title="$page['title']" bg="bg-neutral-900 font-sf-rounded">
+    <div class="p-4 sm:pt-8 pb-24">
+        <div class="max-w-prose mx-auto">
+            <h1 class="text-3xl font-extrabold text-white">{{ $page['title'] }}</h1>
+
+            <div class="mt-4 prose prose-neutral prose-blue !prose-invert">
+                {!! $page['html'] !!}
+            </div>
+        </div>
     </div>
 
     @push('head')
