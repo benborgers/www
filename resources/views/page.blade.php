bec53897e29cd5c0f3da0249ca3b665c6dd1076c
Ben Borgers <borgersbenjamin@gmail.com>
2022-05-05 23:54:03 -0400

    Style pages with translucent blurry nav bar

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,9 +1,25 @@
 <x-layouts.app :title="$page['title']" bg="bg-neutral-900 font-sf-rounded">
-    <div class="p-4 sm:pt-8 pb-24">
+    <a href="{{ route('index') }}" class="block group w-full fixed top-0 inset-x-0 p-4 bg-neutral-900/50 backdrop-blur-xl border-b border-neutral-800">
         <div class="max-w-prose mx-auto">
-            <h1 class="text-3xl font-extrabold text-white">{{ $page['title'] }}</h1>
+            <p @class([
+                'block text-neutral-400 font-medium',
+                'group-hover:underline group-hover:decoration-neutral-600'
+            ])>
+                benborgers.com
+            </p>
+        </div>
+    </a>
 
-            <div class="mt-4 prose prose-neutral prose-rose !prose-invert">
+    <div class="p-4 pt-48 pb-24">
+        <div class="max-w-prose mx-auto">
+            <div>
+                <h1 class="text-3xl font-extrabold text-white">{{ $page['title'] }}</h1>
+                <p class="mt-1 text-neutral-500">
+                    — last updated {{ strtolower($page['updated_at']->format('F Y')) }}
+                </p>
+            </div>
+
+            <div class="mt-16 prose prose-neutral prose-rose !prose-invert">
                 {!! $page['html'] !!}
             </div>
         </div>
