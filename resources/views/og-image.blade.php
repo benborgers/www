a8a88b81b389aec16dabafb68fc77459226bf77f
Ben Borgers <borgersbenjamin@gmail.com>
2022-06-02 22:41:11 -0400

    Make og-image text bigger

--- resources/views/og-image.blade.php
+++ resources/views/og-image.blade.php
@@ -3,6 +3,6 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 <div class="w-[1200px] h-[630px] bg-zinc-900 p-16 grid grid-rows-[1fr,max-content]">
-    <p class="text-zinc-300 text-3xl font-medium">benborgers.com</p>
-    <p class="font-black text-white text-8xl tracking-tight max-w-[1000px]">{{ $title }}</p>
+    <p class="text-zinc-400 text-5xl font-medium">benborgers.com</p>
+    <p class="font-black text-white text-9xl tracking-tight max-w-[1000px]">{{ $title }}</p>
 </div>
