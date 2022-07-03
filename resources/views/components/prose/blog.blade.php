0a1d976cbdbe054193780eb2dd0621445e6f2609
Ben Borgers <borgersbenjamin@gmail.com>
2022-07-03 09:46:18 -0400

    Match corner radius on code blocks

--- resources/views/components/prose/blog.blade.php
+++ resources/views/components/prose/blog.blade.php
@@ -8,7 +8,7 @@
         'prose-img:max-h-[70vh] prose-video:max-h-[70vh]',
         'prose-figcaption:text-center',
         'prose-img:w-auto prose-video:w-auto',
-        'prose-pre:!bg-zinc-100 prose-pre:text-zinc-800',
+        'prose-pre:!bg-zinc-100 prose-pre:text-zinc-800 prose-pre:rounded-xl',
         'before:prose-code:content-none',
         'after:prose-code:content-none',
         '[&_summary]:cursor-pointer [&_summary]:bg-zinc-100 [&_summary]:px-4 [&_summary]:py-2 [&_summary]:rounded-xl',
