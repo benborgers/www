d3985e0056622a85fc8d530b48667cacf1e7cd65
Ben Borgers <borgersbenjamin@gmail.com>
2022-07-03 09:42:10 -0400

    Style `<summary>`

--- resources/views/components/prose/blog.blade.php
+++ resources/views/components/prose/blog.blade.php
@@ -10,7 +10,8 @@
         'prose-img:w-auto prose-video:w-auto',
         'prose-pre:!bg-zinc-100 prose-pre:text-zinc-800',
         'before:prose-code:content-none',
-        'after:prose-code:content-none'
+        'after:prose-code:content-none',
+        '[&_summary]:cursor-pointer [&_summary]:bg-zinc-100 [&_summary]:px-4 [&_summary]:py-2 [&_summary]:rounded-xl',
     ])
 >
   {{ $slot }}
