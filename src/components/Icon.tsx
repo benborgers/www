255e958817b43dc437a5cfc54db7e68769f0d073
Ben Borgers <borgersbenjamin@gmail.com>
2022-07-30 20:26:35 -0400

    Allow arbitrary icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { ArrowDown, TwitterLogo, PaperPlaneTilt } from "phosphor-react";
-
-type IconName = "arrow-down" | "twitter" | "paper-plane-tilt";
-
-const ICONS: Record<IconName, React.ReactNode> = {
-  "arrow-down": ArrowDown,
-  twitter: TwitterLogo,
-  "paper-plane-tilt": PaperPlaneTilt,
-};
+import * as PhosphorIcons from "phosphor-react";
 
 export default function Icon({
   name,
   weight,
   className,
 }: {
-  name: IconName;
+  name: string;
   weight?: "thin" | "light" | "regular" | "bold" | "fill" | "duotone";
   className?: string;
 }) {
-  const Component = ICONS[name];
+  const Component = PhosphorIcons[name];
   return <Component weight={weight} className={className} />;
 }
