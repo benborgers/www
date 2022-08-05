31764afe4f76aa8778c0c278dee34dceb41ff76d
Ben Borgers <borgersbenjamin@gmail.com>
2022-08-05 15:00:06 -0400

    Make syntax highlighting theme `github-light`

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,9 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+    },
+  },
 });
