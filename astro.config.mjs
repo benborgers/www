0c336a63d7a8a7b5ba36e481e36512e2c345f0ae
Ben Borgers <borgersbenjamin@gmail.com>
2022-08-10 21:25:35 -0400

    Add og:image for index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
+  site: "https://benborgers.vercel.app",
   build: {
     format: "file",
   },
