a5805918242105dc6fb0e67ab8aa2c68755c5ec3
Ben Borgers <borgersbenjamin@gmail.com>
2022-08-20 20:26:27 -0400

    Make the NewsletterSignup form work

--- src/components/index/NewsletterSignup.tsx
+++ src/components/index/NewsletterSignup.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Dialog } from "@headlessui/react";
 import { motion, AnimatePresence } from "framer-motion";
-import { X } from "phosphor-react";
+import { X, Confetti } from "phosphor-react";
 
 export default function NewsletterSignup() {
   const [open, setOpen] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   return (
     <>
@@ -27,16 +28,21 @@ export default function NewsletterSignup() {
 
             <div className="fixed bottom-4 inset-x-4 sm:inset-0 flex items-center justify-center">
               <Dialog.Panel
-                className="mx-auto w-full max-w-lg rounded-xl bg-white p-4 sm:px-6 h-[70vh] sm:h-auto"
+                className="mx-auto w-full max-w-lg rounded-xl bg-white p-4 sm:p-6 h-[70vh] sm:h-auto"
                 as={motion.div}
                 initial={{ y: 4 }}
                 animate={{ y: 0 }}
                 exit={{ y: 4 }}
               >
-                <div className="flex items-center justify-between gap-8">
-                  <Dialog.Title className="text-lg font-bold text-neutral-800">
-                    Sign up for my newsletter!
-                  </Dialog.Title>
+                <div className="flex items-start justify-between gap-8">
+                  {!success ? (
+                    <Dialog.Title className="text-lg font-semibold text-neutral-800">
+                      Get updates about my work!
+                    </Dialog.Title>
+                  ) : (
+                    <div />
+                  )}
+
                   <button
                     onClick={() => setOpen(false)}
                     className="outline-neutral-200 outline-offset-2 rounded"
@@ -44,11 +50,100 @@ export default function NewsletterSignup() {
                     <X weight="bold" className="text-neutral-400" />
                   </button>
                 </div>
+
+                {success ? (
+                  <div className="mt-4">
+                    <Confetti weight="duotone" size={24} className="mx-auto" />
+                    <p className="mt-1 text-neutral-800 font-semibold text-center">
+                      Thank you for subscribing!
+                    </p>
+                  </div>
+                ) : (
+                  <>
+                    <div className="mt-2 text-neutral-600">
+                      <p>
+                        I send out an email newsletter every two weeks with
+                        interesting new projects and updates.
+                      </p>
+                    </div>
+
+                    <div className="mt-4 mb-1">
+                      <Form onSuccess={() => setSuccess(true)} />
+                    </div>
+                  </>
+                )}
               </Dialog.Panel>
             </div>
           </Dialog>
         )}
       </AnimatePresence>
     </>
+  );
+}
+
+function Form({ onSuccess }: { onSuccess: () => void }) {
+  const ref = useRef<HTMLFormElement | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const email = ref.current.querySelector('input[type="email"]')?.value;
+
+    setLoading((state) => {
+      state = true;
+      return state;
+    });
+
+    fetch("https://neptune.ooo/benborgers-newsletter/subscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ email }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.errors) {
+          setError(json.errors.email[0]);
+        } else {
+          setError(null);
+          onSuccess();
+        }
+
+        setLoading((state) => {
+          state = false;
+          return state;
+        });
+      });
+  }
+
+  return (
+    <form ref={ref} onSubmit={handleSubmit}>
+      <input
+        type="email"
+        // required
+        placeholder="your email"
+        className="w-full rounded-lg border-neutral-300 bg-neutral-50 shadow-sm focus:border-rose-500 focus:ring-rose-500"
+      />
+
+      {error && <p className="mt-2 text-red-600">{error}</p>}
+
+      <div className="mt-3 flex justify-end">
+        <button
+          className="px-4 py-2.5 rounded-lg shadow bg-rose-600 text-white font-semibold relative"
+          disabled={loading}
+        >
+          <span className={loading ? "opacity-0" : ""}>Subscribe</span>
+
+          {loading && (
+            <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+              <div className="h-5 w-5 border-2 border-white border-t-white/30 rounded-full animate-spin" />
+            </div>
+          )}
+        </button>
+      </div>
+    </form>
   );
 }
