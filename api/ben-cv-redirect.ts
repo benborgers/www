b09d92838299acbd1ccf07df08a88f365f75ec9c
Ben Borgers <borgersbenjamin@gmail.com>
2022-09-28 15:03:28 -0400

    Write a big comment in ben-cv-redirects

--- api/ben-cv-redirect.ts
+++ api/ben-cv-redirect.ts
@@ -1,3 +1,15 @@
+// 2022-09-28
+// Historically, when I switched ben.cv from being its own website to having
+// my blog posts on benborgers.com, I redirected ben.cv/* to
+// benborgers.com/posts/*.
+// Today, I wanted to add a ben.cv/newsletter redirect.
+// So I set up this edge function to be able to add arbitrary redirects
+// (defined as REDIRECTS). But it still falls back to benborgers.com/posts/*
+// to preserve the old behavior.
+// The only change is that ben.cv now redirects to benborgers.com instead of
+// benborgers.com/posts, which I think makes sense given that I don’t write
+// daily blog posts anymore.
+
 export const config = { runtime: "experimental-edge" };
 
 const BASE = "https://benborgers.com";
