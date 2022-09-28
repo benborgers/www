9b62aab52ca9f4e85f0d1d003cd3ba7bc5e0b67d
Ben Borgers <borgersbenjamin@gmail.com>
2022-09-28 15:07:54 -0400

    More padding around newsletter modal

--- src/components/index/NewsletterSignup.tsx
+++ src/components/index/NewsletterSignup.tsx
@@ -42,7 +42,7 @@ export default function NewsletterSignup() {
           >
             <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
 
-            <div className="fixed inset-0 flex items-center justify-center p-2">
+            <div className="fixed inset-0 flex items-center justify-center p-4">
               <Dialog.Panel
                 className="mx-auto w-full max-w-lg rounded-xl bg-white p-4 sm:p-6"
                 as={motion.div}
