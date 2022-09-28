cc5ed6d1d10ee528d7392febde2e3493c9f1f19b
Ben Borgers <borgersbenjamin@gmail.com>
2022-09-28 14:41:16 -0400

    wip

--- api/ben-cv-redirect.ts
+++ api/ben-cv-redirect.ts
@@ -1,3 +1,14 @@
 export const config = { runtime: "experimental-edge" };
 
-export default (req) => new Response("hello from ben-cv-redirect!");
+const BASE = "https://benborgers.com";
+
+export default (req: Request) => {
+  const params = new URL(req.url).searchParams;
+  const path = params.get("path");
+
+  if (path === null || path === "") {
+    return Response.redirect(BASE);
+  }
+
+  new Response("hello from ben-cv-redirect! your path is " + path);
+};
