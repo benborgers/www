02ce1a010831d18ffa5ab29b3b9f99e134227694
Ben Borgers <borgersbenjamin@gmail.com>
2022-12-23 17:30:42 -0800

    New Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,4 @@
-import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
-
-import wikilinks from "./src/remark/wikilinks.mjs";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  site: "https://benborgers.com",
-  build: {
-    format: "file",
-  },
-  markdown: {
-    shikiConfig: {
-      theme: "github-light",
-    },
-    remarkPlugins: [wikilinks],
-    extendDefaultPlugins: true,
-  },
-});
+export default defineConfig({});
