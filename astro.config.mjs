e733e43a09913a43de7f7d0068f9a746f85fc189
Ben Borgers <borgersbenjamin@gmail.com>
2024-12-26 16:58:19 -0500

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://benborgers.com",
-
   integrations: [tailwind(), react(), sitemap()],
-
+  adapter: vercel(),
   trailingSlash: "never",
 
   markdown: {
