27cfce1696fe7c677489d854d924750f2e58b267
Ben Borgers <borgersbenjamin@gmail.com>
2024-12-27 10:56:00 -0500

    Include post content in RSS feed

--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,13 +1,14 @@
 import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
 import getPosts from "../lib/getPosts";
 
+const md = MarkdownIt();
+
 export const GET: APIRoute = async ({ site }) => {
-  const posts = [
-    ...(await getPosts({ includeUnlisted: false })),
-    ...(await getCollection("pages")),
-  ];
+  const posts = await getPosts({ includeUnlisted: false });
 
   return rss({
     title: "Ben Borgers",
@@ -17,6 +18,9 @@ export const GET: APIRoute = async ({ site }) => {
       title: post.data.title,
       pubDate: post.data.date,
       link: `/${post.slug}`,
+      content: sanitizeHtml(md.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
     trailingSlash: false,
   });
