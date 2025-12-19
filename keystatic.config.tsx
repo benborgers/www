2eca0e1690dbc0b39acb4f26ba44df9690a705cb
Ben Borgers <borgersbenjamin@gmail.com>
2025-12-18 23:16:40 -0500

    Add `previewUrl` to Keystatic

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -24,6 +24,7 @@ export default config({
       path: "src/content/posts/*",
       format: { contentField: "body" },
       entryLayout: "content",
+      previewUrl: "/{slug}",
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
         date: fields.date({ label: "Date" }),
